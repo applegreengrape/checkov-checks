c0589130cc58ecd2c7520300de2399f51b14be16
pingzhou <pingzhou.liu@slalom.com>
2020-04-24 18:23:46 +0100

    update

--- myChecks/main.py
+++ myChecks/main.py
@@ -5,7 +5,11 @@ import argparse
 import subprocess
 import sys
 import os.path
-
+from checkov.cloudformation.runner import Runner as cfn_runner
+from checkov.common.runners.runner_registry import RunnerRegistry
+from checkov.common.util.docs_generator import print_checks
+from checkov.terraform.runner import Runner as tf_runner
+from checkov.version import version
 
 def run(filenames):
     """Run 'checkov' command on a dir."""
